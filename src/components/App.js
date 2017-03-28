f73db61c516d042af954a6531391903267b703df
Lars Mortensen (LAM - Adjunkt - Cphbusiness) <lam@cphbusiness.dk>
2017-03-28 09:37:29 +0200

    React Components Added

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import todoStore from "../data/todoStore";
+import TodoList from "./TodoList";
 
 class App extends Component {
   render() {
@@ -9,7 +11,8 @@ class App extends Component {
           <h2>React/MobX Starter Code</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+         <TodoList store={todoStore}/>
+
         </p>
       </div>
     );
